eea57edc613cef8a28997e9133bdf20214283e1a
Ajay Sinha <ajaysinhaa09@gmail.com>
2022-12-13 12:00:19 +0530

    updating app file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/Home";
+import About from "./components/About";
 function App() {
   return (
     <>
@@ -15,6 +16,7 @@ function App() {
         <h1 class="text-blue-500">This is blue text ajaysinhaorigin</h1>
       </div>
       <Home />
+      <About />
     </>
   );
 }
